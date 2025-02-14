b1259a34045f4d346283b0d288f752d3e63dda70
Ryan Bright <ryan@moonriseconsulting.io>
2025-02-14 00:12:21 -0700

    feat(macos): add more macOS preferences

--- hosts/default.nix
+++ hosts/default.nix
@@ -69,13 +69,22 @@ in
         # Reduce delay when showing dock
         autohide-time-modifier = 0.2;
 
-        # Hide recent applications
-        show-recents = false;
-
         # Enable launch animation
         launchanim = true;
 
+        # Show dock on bottom
         orientation = "bottom";
+
+        # Show process indicators
+        show-process-indicators = false;
+
+        # Hide recent applications
+        show-recents = false;
+
+        # Show dynamic icons
+        static-only = false;
+
+        # Set tile size
         tilesize = 48;
       };
 
@@ -83,8 +92,26 @@ in
         # Hide full path in Finder title bar
         _FXShowPosixPathInTitle = false;
 
+        # Sort folders first
+        _FXSortFoldersFirst = true;
+
+        # When performing a search, search the current folder by default
+        FXDefaultSearchScope = "SCcf";
+
         # Hide hidden files
         AppleShowAllFiles = false;
+
+        # Show external hard drives on desktop
+        ShowExternalHardDrivesOnDesktop = false;
+
+        # Show hard drives on desktop
+        ShowHardDrivesOnDesktop = false;
+
+        # Show mounted servers on desktop
+        ShowMountedServersOnDesktop = false;
+
+        # Show removable media on desktop
+        ShowRemovableMediaOnDesktop = false;
       };
 
       trackpad = {
@@ -99,12 +126,65 @@ in
         # Disable "Downloaded from Internet" warnings
         LSQuarantine = false;
       };
+
+      CustomUserPreferences = {
+        NSGlobalDomain = {
+          # Add a context menu item for showing the Web Inspector in web views
+          WebKitDeveloperExtras = true;
+        };
+
+        # Enable auto-update for apps
+        "com.apple.commerce".AutoUpdate = true;
+
+        "com.apple.desktopservices" = {
+          # Avoid creating .DS_Store files on network or USB volumes
+          DSDontWriteNetworkStores = true;
+          DSDontWriteUSBStores = true;
+        };
+
+        "com.apple.print.PrintingPrefs" = {
+          # Automatically quit printer app once the print jobs complete
+          "Quit When Finished" = true;
+        };
+
+        "com.apple.screencapture" = {
+          # Set the location of the screenshot
+          location = "~/My Drive/Screenshots";
+
+          # Set the type of screenshot
+          type = "png";
+        };
+
+        # Prevent Photos from opening automatically when devices are plugged in
+        "com.apple.ImageCapture".disableHotPlug = true;
+
+        "com.apple.SoftwareUpdate" = {
+          # Enable automatic software updates
+          AutomaticCheckEnabled = true;
+
+          # Check for software updates daily, not just once per week
+          ScheduleFrequency = 1;
+
+          # Download newly available updates in background
+          AutomaticDownload = 1;
+
+          # Install System data files & security updates
+          CriticalUpdateInstall = 1;
+        };
+
+        "com.apple.CrashReporter" = {
+          # Disable crash reporter
+          DialogType = "none";
+        };
+      };
     };
 
-    activationScripts.postActivation.text = ''
-      # Disable crash reporter
-      defaults write com.apple.CrashReporter DialogType none
+    activationScripts.postUserActivation.text = ''
+      # Following line should allow us to avoid a logout/login cycle
+      /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+    '';
 
+    activationScripts.postActivation.text = ''
       # Enable firewall with logging and stealth mode
       echo "Configuring firewall..."
       /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
