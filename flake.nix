f1f45ad4182bf96267770c35333db53525bfb1bd
Ryan Bright <ryan@moonriseconsulting.io>
2025-05-26 07:21:13 -0600

    chore(flake): clean up flake config

--- flake.nix
+++ flake.nix
@@ -3,18 +3,16 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
     darwin = {
       url = "github:LnL7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     nix-homebrew = {
       url = "github:zhaofengli/nix-homebrew";
-      # url = "github:zhaofengli-wip/nix-homebrew";
-    };
-    homebrew-bundle = {
-      url = "github:homebrew/homebrew-bundle";
-      flake = false;
     };
     homebrew-core = {
       url = "github:homebrew/homebrew-core";
@@ -24,18 +22,22 @@
       url = "github:homebrew/homebrew-cask";
       flake = false;
     };
+    homebrew-bundle = {
+      url = "github:homebrew/homebrew-bundle";
+      flake = false;
+    };
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      home-manager,
       darwin,
+      home-manager,
       nix-homebrew,
-      homebrew-bundle,
       homebrew-core,
       homebrew-cask,
+      homebrew-bundle,
     }@inputs:
     let
       user = "rbright";
@@ -100,14 +102,18 @@
             {
               nix-homebrew = {
                 inherit user;
+
                 enable = true;
+
+                autoMigrate = true;
+                enableRosetta = true;
+                mutableTaps = false;
+
                 taps = {
                   "homebrew/homebrew-core" = homebrew-core;
                   "homebrew/homebrew-cask" = homebrew-cask;
                   "homebrew/homebrew-bundle" = homebrew-bundle;
                 };
-                mutableTaps = false;
-                autoMigrate = true;
               };
             }
             ./hosts
