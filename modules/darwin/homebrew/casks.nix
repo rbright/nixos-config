47ac88b8e6accd913432ca1725e3fefd10f1f938
Ryan Bright <ryan@moonriseconsulting.io>
2025-06-27 01:43:15 -0600

    feat(casks): add virtualbox and vagrant

--- modules/darwin/homebrew/casks.nix
+++ modules/darwin/homebrew/casks.nix
@@ -19,6 +19,8 @@ _:
   "proxyman" # Web debugging proxy for macOS
   "tableplus" # Modern database management tool
   "tower" # Git client with visual interface
+  "vagrant" # Virtual machine manager for creating and managing virtual environments
+  "virtualbox" # Virtualization software for creating and managing virtual machines
   "zed" # High-performance collaborative code editor
   "zulu@17" # OpenJDK distribution (Java 17)
 
