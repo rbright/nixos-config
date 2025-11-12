543803238a272eda75d31d4b6db613125b78c69d
Ryan Bright <ryan@moonriseconsulting.io>
2025-11-11 18:55:04 -0700

    feat(fonts): add JetBrains Mono, Geist Mono, Lexend, IBM Plex

--- modules/darwin/fonts.nix
+++ modules/darwin/fonts.nix
@@ -7,11 +7,15 @@
   fonts.packages = with pkgs; [
     fira-code
     fira-code-symbols
+    ibm-plex
     inter
     lato
+    lexend
     montserrat
     nerd-fonts.fira-code
+    nerd-fonts.geist-mono
     nerd-fonts.hack
+    nerd-fonts.jetbrains-mono
     quicksand
   ];
 }
