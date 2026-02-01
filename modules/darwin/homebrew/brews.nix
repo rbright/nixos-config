9247245ca4d5b7e3986ac5ee276ff1e9289db429
Ryan Bright <ryan@moonriseconsulting.io>
2026-01-31 23:56:25 -0700

    feat(brews): add sag for voice

--- modules/darwin/homebrew/brews.nix
+++ modules/darwin/homebrew/brews.nix
@@ -1,4 +1,5 @@
 _: [
   "gstreamer" # Development framework for multimedia applications
   "libnice-gstreamer" # GStreamer Plugin for libnice
+  "steipete/tap/sag" # Like the macOS say command, but with a modern voice"
 ]
