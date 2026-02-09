864dba049c844cc375d681cc9442113a0406673c
Ryan Bright <ryan@moonriseconsulting.io>
2026-02-09 07:32:19 -0700

    feat(dock): replace Ghostty with WezTerm

--- modules/darwin/dock/default.nix
+++ modules/darwin/dock/default.nix
@@ -19,7 +19,7 @@
       { path = "/Applications/Sunsama.app/"; }
       { path = "/Applications/Linear.app/"; }
       { path = "/Applications/Obsidian.app/"; }
-      { path = "/Applications/Ghostty.app/"; }
+      { path = "/Applications/WezTerm.app/"; }
       { path = "/Applications/Zed.app/"; }
       { path = "/Applications/Visual Studio Code.app/"; }
       { path = "/Applications/CodexMonitor.app/"; }
